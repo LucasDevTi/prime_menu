a6bd9f14f0dace3142629a19312e85d5e0b13799
LucasDevTi <lmdtechnologyinfo.ux@gmail.com>
2024-10-25 00:13:21 -0300

    model controller migration order_items

---
+++ database/migrations/2024_10_25_030746_create_order_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->string('item_name')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->float('price')->nullable();
+            $table->float('sub_total')->nullable();
+            $table->foreignId('transferred_table_id')->constrained('tables')->onDelete('cascade')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    
+    }
+};
